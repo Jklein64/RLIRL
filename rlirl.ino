527e06ef5d305545450b9ede54e60b11aca754c9
Jason Klein <jklein.64@verizon.net>
2023-03-25 15:03:24 -0400

    Give things cute names

--- rlirl.ino
+++ rlirl.ino
@@ -1,10 +1,44 @@
+#include <Servo.h>
+
+/*
+motors:
+ - drive #1: Danny
+ - drive #2: Derek
+ - mass: Madha
+
+servos:
+ - gimbal #1: Greg
+ - gimbal #2: Gary
+ - steering: Sammy
+*/
+
+#define DANNY_MIN 0
+#define DANNY_MAX 0
+#define DANNY_PIN 3
+
+Servo danny;  // create servo object to control the ESC
+
+int interpolant = 0;  // value from the analog pin
+
+char input;
+
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(9600);
+
+    danny.attach(DANNY_PIN, 1000, 2000);  // (pin, min pulse width, max pulse width in microseconds)
+    delay(5000);
 }
 
 void loop() {
-    digitalWrite(LED_BUILTIN, HIGH);
-    delay(1000);
-    digitalWrite(LED_BUILTIN, LOW);
-    delay(1000);
+    if (Serial.available() > 0) {
+        input = Serial.read();
+        if (input == '=') {
+            interpolant = interpolant + 10;
+        } else if (input == '-') {
+            interpolant = interpolant - 10;
+        }
+        Serial.println(interpolant);
+    }
+
+    danny.write(interpolant);
 }
