693cae9bf729e59a57c165bc8d4b4ef4cdeae2c9
Jason Klein <jklein.64@verizon.net>
2023-03-26 00:57:14 -0400

    Attach steering and one wheel

--- receiver/receiver.ino
+++ receiver/receiver.ino
@@ -16,12 +16,11 @@ servos:
 */
 
 #define DANNY_MIN 85
-#define DANNY_MAX 0
-#define DANNY_PIN 10
+#define DANNY_MAX 200
+#define DANNY_PIN 6
 
-#define SAMMY_MIN 2
-#define SAMMY_MAX 179
-#define SAMMY_PIN 2
+#define SAMMY_CENTER 120
+#define SAMMY_PIN 3
 
 #define GARY_MIN 0    // update this!
 #define GARY_MAX 180  // update this!
@@ -30,9 +29,17 @@ servos:
 Servo danny, sammy, gary;
 
 int
-    steer = (SAMMY_MIN + SAMMY_MAX) / 2,  // SAMMY_MIN to SAMMY_MAX
+    steer = SAMMY_CENTER,
     gimbal_1 = (GARY_MIN + GARY_MAX) / 2,
     throttle = 0;  // 0 to 100, and then interpolate for each motor
+
+// if we have received 7 or more keys
+// without any w, then we must've let go,
+// so start draining the throttle.
+// similar with steering
+int
+    keys_since_last_w = 0,
+    keys_since_last_steer = 0;
 
 RF24 radio(9, 8);
 const byte address[10] = "ADDRESS01";
@@ -41,6 +48,8 @@ void setup() {
     Serial.begin(9600);
 
     gary.attach(GARY_PIN);
+    sammy.attach(SAMMY_PIN);
+    danny.attach(DANNY_PIN, 1000, 2000);
 
     radio.begin();
     radio.openReadingPipe(0, address);
@@ -55,6 +64,8 @@ void loop() {
         radio.read(&key, sizeof(key));
         Serial.println(key);
 
+        keys_since_last_w++;
+
         switch (key) {
             case 'q':
                 gimbal_1--;
@@ -63,10 +74,47 @@ void loop() {
             case 'e':
                 gimbal_1++;
                 break;
+
+            case 'w':
+                throttle++;
+                keys_since_last_w = 0;
+                break;
+
+            case 's':
+                throttle--;
+
+            case 'd':
+                steer++;
+                keys_since_last_steer = 0;
+                break;
+
+            case 'a':
+                steer--;
+                keys_since_last_steer = 0;
+                break;
         }
     }
 
+    gimbal_1 = clamp(gimbal_1, GARY_MIN, GARY_MAX);
+
+    if (keys_since_last_w >= 7 && throttle >= 0)
+        throttle--;
+
+    if (keys_since_last_steer >= 6) {
+        // move steer towards the middle
+        if (steer < SAMMY_CENTER)
+            steer++;
+        else
+            steer--;
+    }
+
+    sammy.write(steer);
+    danny.write(throttle);
     gary.write(gimbal_1);
+}
+
+int clamp(int value, int low, int high) {
+    return max(low, min(value, high));
 }
 
 // void setup() {
